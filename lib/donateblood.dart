daad52a4b8b3ff911d07f5d16680957a33e947d7
ADHARSH45 <adharshmv70@gmail.com>
2024-11-17 15:53:07 +0530

    donor submisiion implemented

--- lib/donateblood.dart
+++ lib/donateblood.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DonateBloodScreen extends StatefulWidget {
   @override
@@ -6,7 +7,70 @@ class DonateBloodScreen extends StatefulWidget {
 }
 
 class _DonateBloodScreenState extends State<DonateBloodScreen> {
-  DateTime? _selectedDate;
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _locationController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _healthNotesController = TextEditingController();
+  String? _bloodGroup;
+  DateTime? _lastDonationDate;
+
+  Future<void> _submitDonorData() async {
+    if (_formKey.currentState!.validate()) {
+      // Ensure the last donation date is more than 3 months ago
+      if (_lastDonationDate != null) {
+        final currentDate = DateTime.now();
+        final difference = currentDate.difference(_lastDonationDate!).inDays;
+
+        if (difference < 90) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text(
+                    'You can donate blood only after 3 months from your last donation.')),
+          );
+          return;
+        }
+      }
+
+      // Gather donor data
+      final donorData = {
+        'name': _nameController.text,
+        'bgroup': _bloodGroup,
+        'location': _locationController.text,
+        'phonenumber': _phoneController.text,
+        'email':
+            _emailController.text.isNotEmpty ? _emailController.text : null,
+        'hnote': _healthNotesController.text,
+        'lastdate': _lastDonationDate?.toIso8601String(),
+        'createdAt': Timestamp.now(),
+      };
+
+      try {
+        // Save donor data to Firestore
+        await FirebaseFirestore.instance.collection('donor').add(donorData);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Donor data submitted successfully!')),
+        );
+
+        // Clear form after submission
+        _formKey.currentState!.reset();
+        _nameController.clear();
+        _locationController.clear();
+        _phoneController.clear();
+        _emailController.clear();
+        _healthNotesController.clear();
+        setState(() {
+          _bloodGroup = null;
+          _lastDonationDate = null;
+        });
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error submitting donor data: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,129 +81,147 @@ class _DonateBloodScreenState extends State<DonateBloodScreen> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Fill in your details to become a donor:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              // Blood Group Dropdown
-              Text('Blood Group:', style: TextStyle(fontSize: 16)),
-              SizedBox(height: 10),
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Fill in your details to become a donor:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                items: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
-                    .map((group) => DropdownMenuItem(
-                          value: group,
-                          child: Text(group),
-                        ))
-                    .toList(),
-                onChanged: (value) {
-                  // Handle blood group selection
-                },
-                hint: Text('Select Blood Group'),
-              ),
-              SizedBox(height: 20),
-              // Last Donation Date
-              Text('Last Donation Date:', style: TextStyle(fontSize: 16)),
-              SizedBox(height: 10),
-              TextFormField(
-                readOnly: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: _selectedDate == null
-                      ? 'Select a date'
-                      : '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}',
-                  suffixIcon: Icon(Icons.calendar_today),
+                SizedBox(height: 20),
+                // Name Field
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter your full name',
+                  ),
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Name is required'
+                      : null,
                 ),
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: _selectedDate ?? DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime.now(),
-                  );
-                  if (pickedDate != null) {
+                SizedBox(height: 20),
+                // Blood Group Dropdown
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Blood Group',
+                  ),
+                  items: ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']
+                      .map((group) => DropdownMenuItem(
+                            value: group,
+                            child: Text(group),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
                     setState(() {
-                      _selectedDate = pickedDate;
+                      _bloodGroup = value;
                     });
-                  }
-                },
-              ),
-              SizedBox(height: 20),
-              // Location
-              Text('Location:', style: TextStyle(fontSize: 16)),
-              SizedBox(height: 10),
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your location',
-                ),
-              ),
-              SizedBox(height: 20),
-              // Contact Information
-              Text('Contact Information:', style: TextStyle(fontSize: 16)),
-              SizedBox(height: 10),
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your name',
-                  labelText: 'Name',
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your phone number',
-                  labelText: 'Phone Number',
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your email (optional)',
-                  labelText: 'Email',
-                ),
-              ),
-              SizedBox(height: 20),
-              // Health Notes
-              Text('Health Notes (optional):', style: TextStyle(fontSize: 16)),
-              SizedBox(height: 10),
-              TextFormField(
-                maxLines: 3,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Mention any health conditions or medications',
-                ),
-              ),
-              SizedBox(height: 30),
-              // Submit Button
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Handle form submission
                   },
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                    backgroundColor: Colors.red,
+                  validator: (value) =>
+                      value == null ? 'Please select a blood group' : null,
+                  value: _bloodGroup,
+                ),
+                SizedBox(height: 20),
+                // Last Donation Date
+                TextFormField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Last Donation Date',
+                    hintText: _lastDonationDate == null
+                        ? 'Select a date'
+                        : '${_lastDonationDate!.day}/${_lastDonationDate!.month}/${_lastDonationDate!.year}',
+                    suffixIcon: Icon(Icons.calendar_today),
                   ),
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime.now(),
+                    );
+                    if (pickedDate != null) {
+                      setState(() {
+                        _lastDonationDate = pickedDate;
+                      });
+                    }
+                  },
+                  validator: (value) => _lastDonationDate == null
+                      ? 'Please select your last donation date'
+                      : null,
+                ),
+                SizedBox(height: 20),
+                // Location Field
+                TextFormField(
+                  controller: _locationController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Location',
+                    hintText: 'Enter your location',
+                  ),
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Location is required'
+                      : null,
+                ),
+                SizedBox(height: 20),
+                // Contact Information
+                TextFormField(
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone Number',
+                    hintText: 'Enter your phone number',
+                  ),
+                  validator: (value) => value == null || value.isEmpty
+                      ? 'Phone number is required'
+                      : null,
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter your email (optional)',
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                // Health Notes Field
+                TextFormField(
+                  controller: _healthNotesController,
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Health Notes (optional)',
+                    hintText: 'Mention any health conditions or medications',
+                  ),
+                ),
+                SizedBox(height: 30),
+                // Submit Button
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _submitDonorData,
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      backgroundColor: Colors.red,
+                    ),
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
